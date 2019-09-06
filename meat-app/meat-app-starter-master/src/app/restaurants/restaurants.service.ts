ca3a1deadb660788ed2cb1e20b92eaf6ce119149
Bruno Vedoveto Leandro <brunovl@lnxcit024120.cit>
2019-09-06 09:51:54 -0300

    Tratamento de erro

--- meat-app/meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app/meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -1,10 +1,13 @@
 import { Restaurant } from "./restaurant/restaurant.model";
+import { MEAT_API } from "app/app.api";
+import { ErrorHandler } from "app/app.error-handler";
+
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
-import { MEAT_API } from "app/app.api";
 
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map"
+import "rxjs/add/operator/catch"
 
 @Injectable()
 
@@ -15,5 +18,6 @@ export class RestaurantService {
     getRestaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
                         .map(response => response.json())
+                        .catch(ErrorHandler.handleError)
     }
 }
