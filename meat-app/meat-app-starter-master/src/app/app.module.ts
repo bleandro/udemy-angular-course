d6177f995d95de5f7eecc7dd129d7bbb580bf189
Bruno Vedoveto Leandro <brunovl@lnxcit024120.cit>
2019-09-09 09:54:03 -0300

    Utilizando ngModel/ngForm

--- meat-app/meat-app-starter-master/src/app/app.module.ts
+++ meat-app/meat-app-starter-master/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -38,6 +39,7 @@ import { OrderComponent } from './order/order.component'
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantService, ShoppingCartService],
